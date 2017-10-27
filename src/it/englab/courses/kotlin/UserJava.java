5d13e0d35e3d9aea8867f266f00568e9d19db899
spawn150 <spawn150@gmail.com>
2017-10-27 21:13:13 +0200

    data classes

---
+++ src/it/englab/courses/kotlin/UserJava.java
@@ -0,0 +1,46 @@
+package it.englab.courses.kotlin;
+
+public class UserJava {
+
+    private String name;
+    private int age;
+
+    public UserJava(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserJava userJava = (UserJava) o;
+
+        if (age != userJava.age) return false;
+        return name != null ? name.equals(userJava.name) : userJava.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "UserJava{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/it/englab/courses/kotlin/data/data_class.kt
@@ -0,0 +1,11 @@
+package it.englab.courses.kotlin.data
+
+data class User(val name: String, val age: Int){
+    var sex : Char = 'M'
+}
+
+fun main(args: Array<String>) {
+    val user = User("Francesco", 27)
+    user.sex = 'F'
+    println("User $user")
+}
