018351e173e96fd171638669b2b40e95041b676e
spawn150 <spawn150@gmail.com>
2017-10-29 12:23:38 +0100

    esempio per il metodo "copy()"

--- src/it/englab/courses/kotlin/data/data_class.kt
+++ src/it/englab/courses/kotlin/data/data_class.kt
@@ -1,9 +1,13 @@
 package it.englab.courses.kotlin.data
 
-data class User(val name: String, val age: Int, var sex : Char = 'M')
+data class User(val name: String, val age: Int, var sex: Char = 'M')
 
 fun main(args: Array<String>) {
-    val user = User("Francesco", 27)
+    val user = User("Francesca", 27)
     user.sex = 'F'
     println("User $user")
+
+    val clone = user.copy(name = "Francesco", sex = 'M')
+    println("New user $clone")
+    
 }
