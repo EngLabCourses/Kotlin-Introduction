69e8837136c37dbdf4d46c85f394ce82e45e3301
spawn150 <spawn150@gmail.com>
2017-11-07 17:22:00 +0100

    extension function con mio oggetto User

--- src/it/englab/courses/kotlin/extension_functions.kt
+++ src/it/englab/courses/kotlin/extension_functions.kt
@@ -1,11 +1,20 @@
 package it.englab.courses.kotlin
 
+import it.englab.courses.kotlin.data.User
+
 fun main(args: Array<String>) {
     val value = "value"
 
     println("E' uguale a 'value'? ${value.isEqualToValue()} [$value]")
+
+    val user = User("Francesco", 29)
+    user.isAuthenticated()
 }
 
 fun String?.isEqualToValue() : Boolean {
     return this.equals("value")
+}
+
+fun User.isAuthenticated() : Boolean {
+    return true
 }
