aea74a0ecbe821ba21b03eab6adca86b7c7b6f69
spawn150 <spawn150@gmail.com>
2018-02-07 17:33:52 +0100

    aggiunto esempio di function type

--- src/it/englab/courses/kotlin/hof_and_lambdas.kt
+++ src/it/englab/courses/kotlin/hof_and_lambdas.kt
@@ -1,10 +1,17 @@
 package it.englab.courses.kotlin
 
 fun main(args: Array<String>) {
+
+    println("is more than five chars? ${isMoreThanFive("loooong!")}")
+
     wrap(::printBlock)
     println("Risultato ${wrap {
         mathBlock(4)
     }}")
+}
+
+val isMoreThanFive : (String) -> Boolean = {
+    value -> value.length > 5
 }
 
 fun mathBlock(counter: Int) = counter + 1
