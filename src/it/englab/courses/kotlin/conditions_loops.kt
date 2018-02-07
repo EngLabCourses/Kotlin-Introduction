a60029e511d548e14eb7994e68454a661c72a22d
spawn150 <spawn150@gmail.com>
2018-02-07 17:00:22 +0100

    aggiunti esempi di for e uso delle expressions negli if e when

--- src/it/englab/courses/kotlin/conditions_loops.kt
+++ src/it/englab/courses/kotlin/conditions_loops.kt
@@ -1,12 +1,26 @@
 package it.englab.courses.kotlin
 
 fun main(args: Array<String>) {
+
+    println(exampleIfAsExpression(false))
+
     exampleSmartCast("Esempio di Smart Cast")
     exampleSmartCast(29)
+
+    exampleWithWhen(3)
+    exampleWithWhen(27)
+    exampleWithWhen(3.4)
 
     exampleWithMatching("Esempio di Matching con When")
     exampleWithMatching(39)
     exampleWithMatching(null)
+
+    exampleWithFor()
+    exampleWithForAdvanced()
+}
+
+fun exampleIfAsExpression(condition: Boolean): String {
+    return if (condition) "true as String" else "false as String"
 }
 
 fun exampleSmartCast(obj: Any) {
@@ -17,10 +31,35 @@ fun exampleSmartCast(obj: Any) {
     }
 }
 
+fun exampleWithWhen(x: Any) {
+    val y = when (x) { 0 -> "is zero"
+        1 -> "is one"
+        2, 3 -> "two or three"
+        in 0..100 -> "between 0 and 100"
+        is Int -> "is Int"
+        is Double -> "is Double"
+        else -> "else block"
+    }
+
+    println("value of y is $y")
+}
+
 fun exampleWithMatching(obj: Any?) {
     when (obj) {
         is String -> println("Value is $obj and length is ${obj.length}")
         is Int -> println("Value is $obj")
         else -> println("Obj is not a String")
+    }
+}
+
+fun exampleWithFor() {
+    for ((i, c) in "engLab".withIndex()) {
+        println("$i: $c")
+    }
+}
+
+fun exampleWithForAdvanced() {
+    (0 until "engLab".length).forEachIndexed {
+        index, value -> println("$index: " + "engLab"[value])
     }
 }
